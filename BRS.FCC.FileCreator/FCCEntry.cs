51133b9e41eea0a63bcd49e2db65c3c69e3efa15
lanthosbrs <brian@liquidfuture.net>
2023-02-23 16:19:37 -0500

    Add project files.

---
+++ BRS.FCC.FileCreator/FCCEntry.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BRS.FCC.FileCreator
+{
+    internal class FCCEntry
+    {
+        public FCCEntry( string locationId)
+        {
+            provider_id = "430076";
+            brand_name = "Starlink";
+            technology = "61";
+            data_vintage = "2022-02-23";
+            category_code = "5";
+            request_date = "2022-02-23";
+            request_method_code = "3";
+            evidence_description = "Starlinks website shows that this area is not covered yet";
+            location_id = locationId;
+        }
+
+        public string provider_id { get; set; } 
+        public string brand_name { get; set; }
+        public string technology { get; set; }
+        public string? location_id { get; set; }
+        public string data_vintage { get; set; }
+        public string category_code { get; set; }
+        public string request_date { get; set; }
+        public string request_method_code { get; set; }
+        public string evidence_description { get; set; }
+                  
+    }
+}
---
+++ BRS.FCC.FileCreator/Program.cs
@@ -0,0 +1,29 @@
+﻿using BRS.FCC.FileCreator;
+using CsvHelper;
+using System.Globalization;
+
+
+Console.WriteLine("Hello, World! Lets create a file");
+
+
+int startingNumber = 1349428793;
+int endingNumber   = 1349459425;
+
+int count = endingNumber - startingNumber;
+
+IEnumerable<int> allLocationCodes = Enumerable.Range(startingNumber, count);
+var records = new List<FCCEntry>();
+
+foreach (var i in allLocationCodes)
+{
+    records.Add(new FCCEntry(i.ToString()));
+}
+
+
+using (var writer = new StreamWriter("C:\\Users\\brian\\OneDrive\\Desktop\\fcc_submission.csv"))
+using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+{
+    csv.WriteRecords(records);
+}
+
+Console.WriteLine("Finished!");
